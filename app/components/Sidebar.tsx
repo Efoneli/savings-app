7565a2066596e83c0f12b6e9e47b483f5323c1d4
efoneli <abbaefoneli@gmail.com>
2025-04-02 15:27:12 +0100

    auth added

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaHome, FaWallet, FaChartLine, FaList, FaCog, FaBell, FaUser } from "react-icons/fa";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 interface MenuItem {
   id: string;
@@ -40,16 +42,80 @@ const Sidebar: React.FC = () => {
 
 
 
-const Header: React.FC = () => {
+// const Header: React.FC = () => {
+//   return (
+//     <div className="w-full bg-white text-gray-600 shadow-md p-4 flex justify-between items-center">
+//       <h2 className="text-xl font-bold">Dashboard</h2>
+//       <div className="flex items-center gap-4">
+//         <FaBell className="text-gray-700 cursor-pointer" />
+//         <Link href={'/settings'}>
+//         <FaUser className="text-gray-700 cursor-pointer" />
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+const Header = () => {
+  const { user, isLoading } = useUser();
+  console.log(user, "this is the user");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
+
+  function isString(value: string | null | undefined): value is string {
+    return typeof value === "string";
+  }
   return (
-    <div className="w-full bg-white text-gray-600 shadow-md p-4 flex justify-between items-center">
-      <h2 className="text-xl font-bold">Dashboard</h2>
-      <div className="flex items-center gap-4">
-        <FaBell className="text-gray-700 cursor-pointer" />
-        <Link href={'/settings'}>
-        <FaUser className="text-gray-700 cursor-pointer" />
-        </Link>
-      </div>
+    <div>
+      {" "}
+      <header className="bg-pink-200 p-4 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link href={"/"} className="text-xl font-semibold">
+            MiniGit
+          </Link>
+          <div className="flex items-center">
+            {user && (
+              <div className="relative">
+                {user && (
+                  <Image
+                    src={isString(user?.picture) ? user?.picture : ""}
+                    alt="User Avatar"
+                    className="w-8 h-8 rounded-full cursor-pointer"
+                    onClick={toggleDropdown}
+                    width={40}
+                    height={40}
+                  />
+                )}
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 py-2 bg-white border border-gray-300 rounded shadow-lg">
+                    <div className="px-4 py-2">
+                      <p className="font-bold text-black">{user.name}</p>
+                    </div>
+                    <Link
+                      href="/api/auth/logout"
+                      className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-blue-100"
+                    >
+                      Logout
+                    </Link>
+                  </div>
+                )}
+              </div>
+            )}
+            {!isLoading && !user && (
+              <Link
+                href="/api/auth/login"
+                className="bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
